90a31c3242101a43334e49ec8be5240ee53fba43
Ssaava <ssavaemma4@gmail.com>
2024-10-09 11:30:15 +0300

    added footer to users page

--- src/layouts/UserLayout.tsx
+++ src/layouts/UserLayout.tsx
@@ -1,12 +1,14 @@
 import {Outlet} from "react-router-dom";
 import UserHeader from "@/components/user-components/UserHeader.tsx";
+import UserFooter from "@/components/user-components/UserFooter.tsx";
 
 const UserLayout = ()=>{
     return (
         <>
-            <div>
+            <div className={"flex flex-col min-h-screen"}>
                 <UserHeader/>
                 <Outlet />
+               <UserFooter/>
             </div>
         </>
     )
