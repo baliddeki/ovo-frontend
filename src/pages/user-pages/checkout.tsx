3de9498fef124a0ba3115bc35ff17799444160f3
Ssaava <ssavaemma4@gmail.com>
2024-10-28 11:17:12 +0300

    checkout page

--- src/pages/user-pages/checkout.tsx
+++ src/pages/user-pages/checkout.tsx
@@ -1,9 +1,15 @@
 const Checkout = ()=>{
     return (
         <>
-            Checkout Page
+            <div
+                className={"md:min-h-[calc(100vh-6.25rem)] w-screen pb-16 section inline-padding flex flex-col bg-[#F2F4F6]"}>
+                <div className={"bg-white w-full p-6 md:p-12 mx-auto mt-16 vertical-spacing"}>
+                    <div className={"border"}>
+                        User Checkout page
+                    </div>
+                </div>
+            </div>
         </>
     )
 }
-
-export default Checkout
+ export default Checkout
